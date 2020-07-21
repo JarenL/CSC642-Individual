aff49d57f8595fe403b2c4bb5dd434aa98add8a6
Jaren Lynch <jaren.d.lynch@gmail.com>
2020-07-20 19:36:27 -0700

    Updates

---
+++ precache-manifest.2fbca9ef566f214ece83d52932eadf24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d12613f1a06d6154de0f09b8be2ff21",
+    "url": "/CSC642-Individual/index.html"
+  },
+  {
+    "revision": "badb30d52a2d6a098768",
+    "url": "/CSC642-Individual/static/css/main.d18dee0f.chunk.css"
+  },
+  {
+    "revision": "98beefbf30ee97c69105",
+    "url": "/CSC642-Individual/static/js/2.05d5fbff.chunk.js"
+  },
+  {
+    "revision": "3a08a7c8f8e6231522abe29a929e6912",
+    "url": "/CSC642-Individual/static/js/2.05d5fbff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "badb30d52a2d6a098768",
+    "url": "/CSC642-Individual/static/js/main.f452bb99.chunk.js"
+  },
+  {
+    "revision": "a06b3ad480950d1453d2",
+    "url": "/CSC642-Individual/static/js/runtime-main.f6c487f8.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/CSC642-Individual/static/media/logo.8dad8028.svg"
+  }
+]);
